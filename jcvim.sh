788e8a14c0fe31db0a5b524d3dc3a3e9b4419d07
Jaime Cooper <jcooper2112@gmail.com>
2018-08-11 12:41:07 -0500

    first commit

---
+++ jcvim.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# check if vimfiles exists, back it up and remove it.
+if [ -e ~/vimfiles exists ]
+then
+    tar -zcvf vimfiles_bak.tar.gz ~/vimfiles
+    rm -rf ~/vimfiles
+fi
+
+# make a new vimfiles directory
+mkdir ~/vimfiles
+
+# copy my git files over to the vimfiles dir
+cp * ~/vimfiles
+
+# git support stuff
+git clone https://github.com/sjl/badwolf.git ~/vimfiles/git/badwolf
+git clone https://github.com/scrooloose/nerdtree.git ~/vimfiles/git/nerdtree
+
+# copy files to correct dirs
+cp -r ~/vimfiles/git/nerdtree/autoload ~/vimfiles/
+cp -r ~/vimfiles/git/badwolf/colors ~/vimfiles/
+cp -r ~/vimfiles/git/badwolf/contrib ~/vimfiles/
+
+curl -LSso ~/vimfiles/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# check if .vimrc already exists and back it up
+if [ -e ~/.vimrc exists ]
+then
+    mv ~/.vimrc ~/.vimrc_bak
+fi
+
+# make symbolic links
+ln -s ~/vimfiles/vimfiles ~/.vim
+ln -s ~/vimfiles/vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,19 @@
+execute pathogen#infect()
+colorscheme badwolf	"selected colorscheme
+syntax enable		"enable syntax processing
+filetype on
+set tabstop=4		"number of visual spaces per tab
+set softtabstop=4	"number of spaces in tab when editing
+set expandtab		"tabs are spaces
+set number 		"show line numbers
+set showcmd		"show command in bottom bar
+set cursorline		"highlights current line
+set wildmenu		"visual autocomplete for command menu
+set hlsearch        "hightlight search
+
+"escape to clear search highlights
+nnoremap <silent><Esc> :nohlsearch<bar>:echo<CR>
+set showmatch       "show matching parenthesis
+
+"swtiches windows between last file.
+nnoremap <Leader><Leader> :e#<CR>
