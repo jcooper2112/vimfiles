d20466e5b377933c4d3f44ca53cf60f86141850e
Jaime Cooper <jcooper2112@gmail.com>
2018-08-11 13:31:30 -0500

    Fixed some more bugs

--- jcvim.sh
+++ jcvim.sh
@@ -17,10 +17,12 @@ cp vimrc ~/vimfiles
 git clone https://github.com/sjl/badwolf.git ~/vimfiles/git/badwolf
 git clone https://github.com/scrooloose/nerdtree.git ~/vimfiles/git/nerdtree
 
-# copy files to correct dirs
-cp -r ~/vimfiles/git/nerdtree/autoload ~/vimfiles/
-cp -r ~/vimfiles/git/badwolf/colors ~/vimfiles/
-cp -r ~/vimfiles/git/badwolf/contrib ~/vimfiles/
+# move files to correct dirs
+mkdir ~/vimfiles/bundle
+mkdir ~/vimfiles/autoload
+mv  ~/vimfiles/git/nerdtree ~/vimfiles/bundle/nerdtree
+mv  ~/vimfiles/git/badwolf/colors ~/vimfiles/
+mv  ~/vimfiles/git/badwolf/contrib ~/vimfiles/
 
 curl -LSso ~/vimfiles/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
@@ -31,5 +33,5 @@ then
 fi
 
 # make symbolic links
-ln -s ~/vimfiles/vimfiles ~/.vim
+ln -s ~/vimfiles ~/.vim
 ln -s ~/vimfiles/vimrc ~/.vimrc
